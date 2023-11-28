d567d3d96a0e45c3ac498b8e97877c3d25687f63
Hubert Suchorowski <hubert.suchorowski1@gmail.com>
2023-11-28 11:40:34 +0100

    Added section with current services

--- myapp/src/lib/components/services.svelte
+++ myapp/src/lib/components/services.svelte
@@ -1,3 +1,23 @@
+<script lang="ts">
+	const oferty = [
+		{
+			src: '/1.webp',
+			title: 'Zadbaj o posiłki dla pracowników',
+			description: 'Utwórz konto firmowe'
+		},
+		{
+			src: '/2.webp',
+			title: 'Ty zapewniasz menu, a my dostawę',
+			description: 'Dodaj swoją restaurację'
+		},
+		{
+			src: '/3.webp',
+			title: 'Realizuj dostawy z Uber Eats',
+			description: 'Zarejestruj się, aby realizować dostawy'
+		}
+	];
+</script>
+
 <main>
 	<section class="main-content">
 		<div class="header">
@@ -9,15 +29,34 @@
 				<button>Pobierz aplikacje</button>
 			</div>
 		</div>
+		<div class="services">
+			{#each oferty as index}
+				<img class="services-img" src={index.src} />
+				<h2>{index.title}</h2>
+				<p>{index.description}</p>
+			{/each}
+		</div>
 	</section>
 	<slot />
 </main>
 
 <style lang="scss">
+
+
+	h2{
+		font-size: 22px;
+		font-weight: 700;
+	}
+
+	p{
+		font-weight: 550;
+		text-decoration:underline;
+	}
+
 	.main-content {
 		height: 300vh;
 		.header {
-			height: 196px;
+			height: 140px;
 			margin: 48px 16px;
 			display: flex;
 			flex-direction: column;
@@ -33,12 +72,21 @@
 					width: 64px;
 				}
 			}
-			Button{
+			Button {
 				height: 56px;
 				width: 100%;
 				border-radius: 8px;
 				font-size: 18px;
 				font-weight: 600;
+			}
+		}
+		.services {
+			margin: 0px 16px;
+			display: flex;
+			flex-direction: column;
+			gap: 12px;
+			.sevices-img{
+				margin-top: 20px;
 			}
 		}
 	}
