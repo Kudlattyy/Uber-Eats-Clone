724a0de590d906829b7083438f9e60a34dab57dd
Hubert Suchorowski <hubert.suchorowski1@gmail.com>
2023-11-29 12:53:12 +0100

    Started work on footer section

---
+++ myapp/src/lib/components/footer.svelte
@@ -0,0 +1,41 @@
+<main>
+	<hr />
+	<div class="main-content">
+		<img src="/Logo.svg" />
+		<div class="Stores">
+			<img class="Stores-style" src="/AppStore.svg" />
+			<img class="Stores-style" src="/GoogleStore.png" />
+		</div>
+		<div class="Tools">
+			<div class="Help">
+
+			</div>
+			<div class="Other">
+				
+			</div>
+		</div>
+	</div>
+	<slot />
+</main>
+
+<style lang="scss">
+	hr {
+		border: solid 0.1px;
+		width: 100vw;
+		color: #e5e5e5;
+	}
+	.main-content {
+		height: 100vh;
+		margin-top: 64px;
+		padding: 64px 16px 80px;
+		.Stores{
+			margin-top: 50px;
+			display: flex;
+			gap: 20px;
+			.Stores-style{
+				width: 135;
+				height: 40px;
+			}
+		}
+	}
+</style>
